6b0f9a1248107061a0dded419fd10a762a0173b0
Thu-0810 <thupham2040@gmail.com>
2025-12-30 22:25:15 +0700

    Initial commit

---
+++ src/main/java/com/example/artwebsitebe/ArtwebsitebeApplication.java
@@ -0,0 +1,13 @@
+package com.example.artwebsitebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtwebsitebeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtwebsitebeApplication.class, args);
+    }
+
+}
